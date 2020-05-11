553d591f01a1fc90bbb7561e5a39bb94f1b7ab12
Zoe Xanos <zxanos@gmail.com>
2020-05-11 02:08:42 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,6 @@
-
 def reverse_each_word(sentence)
-  array_sen = sentence.split(" ")
-  starting_here = []
-  
-  array_sen.each do |word|
-    word.reverse!
-    starting_here << word.split(" ")
-  end
-  starting_here.flatten
-  final = starting_here.join(" ")
-  final
-end
-
-def reverse_with_collect(sentence)
   new_array = sentence.split(" ")
   new_array.map { |x| x }
-  new_array.collect { |x| x.reverse }
-  new_array
+  new_array.collect { |x| x.reverse! }
+  new_array.join(" ")
 end
